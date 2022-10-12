7083d5f58d255d553c60108bae5aa00175e8c7b8
Yury Siamionau <up.semenov@gmail.com>
2022-10-12 02:24:34 +0200

    Create Test.js

---
+++ Test.js
@@ -0,0 +1,18 @@
+let str = 'Hi'
+let bool = 'True'
+let num = 12
+console.log(str + bool)
+console.log(str + num)
+console.log(num + bool)
+
+console.log(str * bool)
+console.log(str * num)
+console.log(num * bool)
+
+console.log(str / bool)
+console.log(str / num)
+console.log(num / bool)
+
+console.log(Number(bool))
+console.log(String(num))
+console.log(Boolean(num))
